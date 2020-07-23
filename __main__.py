6c1c5991026191ebbf0f55bd12d635b59c46086f
yung-ber software studios <57375758+yung-ber@users.noreply.github.com>
2020-07-23 12:11:01 -0400

    fine-tuning

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 from microbit import *
 import speech
-checklist=['feed the animals','take a shower','put deoderant on', 'hang up towel', 'make bed', 'tidy room for 10 minutes:timer(10)', 'eat your breakfast', 'dishes 2 dish washer', 'wipe table', 'brush teeth:timer(2)']
+checklist=['feed the animals','take a shower','put deoderant on', 'hang up towel', 'make bed', 'tidy room for ten minutes:timer(600000)', 'eat your breakfast', 'dishes 2 dish washer', 'wipe table', 'brush teeth 4 2 minutes:timer(120000)']
 # put your own checklist in the checklist array
 def timer(time):
     sec=time
@@ -9,11 +9,11 @@ def timer(time):
         sleep(1000)
         display.show(str(sec),wait=False)
 display.scroll('Are you ready 4 the checklist?')
-speech.say('YEET')
+speech.say('are you ready 4 the checklist')
 display.show(Image.HEART)
 sleep(1000)
 display.scroll('PRESS A')
-speech.say('to start doing the checklist, press the A button')
+speech.say('to start doing the checklist, press the aye button')
 display.show(Image.ARROW_W)
 while not button_a.is_pressed():
     continue
@@ -26,8 +26,8 @@ for item in checklist:
         item,ree_is_cool=item.split(':')
     speech.say(item)
     display.scroll(item)
-    sleep(5000)
-    speech.say('press the A button when you are ready to start doing '+item)
+    sleep(1000)
+    speech.say('press the aye button when you are ready to start doing '+item)
     while not button_a.is_pressed():
         continue
     if ree: # if ree is a true fact (which it is, see line 25)
